5234948b00c3881f2d0d7db38f74abcdb5dd44f1
webdevelopersinkenya <mir22006@byui.edu>
2023-10-12 23:50:58 +0300

    c

--- Js/utils.mjs
+++
@@ -1,8 +0,0 @@
-// utils.mjs
-
-export function getParam(param) {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    return urlParams.get(param);
-  }
-  
