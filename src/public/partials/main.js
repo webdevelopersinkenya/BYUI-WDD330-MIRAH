8d48dc87ec9fa0e6d6c241a401b5e41cd8586ac9
webdevelopersinkenya <mir22006@byui.edu>
2023-10-15 22:43:20 +0300

    rename

--- src/public/partials/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-import { loadHeaderFooter } from './utils.js';
-
-// Assuming your templates are in the 'partials' directory
-const headerPath = '/partials/header.html';
-const footerPath = '/partials/footer.html';
-
-// Call the function to load and render header and footer
-loadHeaderFooter(headerPath, footerPath);
